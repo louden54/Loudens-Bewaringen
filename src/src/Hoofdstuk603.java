ac5c5264d3a610798a0f9c782d80f67cadf09f72
louden54 <louden_bout@msn.com>
2016-09-15 13:27:20 +0200

    alles tot hoofdstuk 6 praktijk

--- src/src/Hoofdstuk603.java
+++ src/src/Hoofdstuk603.java
@@ -1,7 +1,29 @@
 package src;
 
-/**
- * Created by loude on 15-9-2016.
- */
-public class Hoofdstuk603 {
+import java.awt.*;
+import java.applet.*;
+
+
+public class Hoofdstuk603 extends Applet {
+    int a;
+    int b;
+
+
+    int uitkomst;
+    int uitkomst2;
+    int uitkomst3;
+
+    public void init() {
+        a = 300000000;
+        b = 2000000000;
+
+        uitkomst = (a + b);
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("de uitkomst = : " + uitkomst, 20, 20);
+
+
+    }
 }
